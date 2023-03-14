617959d082dbbdd04123631d96c43da33522c528
Joshua Davian <joshuadavian@gmail.com>
2023-03-14 14:07:20 +0700

    models

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'logo', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function teams()
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function roles()
+    {
+        return $this->hasMany(Role::class);
+    }
+}
---
+++ app/Models/Employee.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'email', 'gender', 'age', 'photo', 'phone', 'role_id', 'team_id', 'is_verified', 'verified_at'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
